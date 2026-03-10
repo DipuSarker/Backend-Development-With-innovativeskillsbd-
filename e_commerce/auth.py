127dd7e0fc78966757ee462ebfa936f78781a3a6
unknown <mr.d.sarker@gmail.com>
2026-03-10 21:45:12 +0600

    Signup complete

--- e_commerce/auth.py
+++ e_commerce/auth.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
+from user.models import User
+from django.contrib import messages
 
 def signup(request):
-    
+    if request.method == "POST":
+        data = request.POST
+        name = data.get('name', '')
+        email = data.get('email', '')
+        password = data.get('password', '')
+        confirm_password = data.get('confirm_password', '')
+        
+        if password != confirm_password:
+            messages.success(request, 'Password and confirm password not match .')
+            return render(request, 'pages/register.html')
+        User.objects.create(full_name=name, email=email, password=confirm_password)
     return render(request, 'pages/register.html')
