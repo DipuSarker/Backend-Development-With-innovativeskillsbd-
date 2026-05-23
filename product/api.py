537a805a02479b762df019fbdaf47e30a95d3ade
DipuSarker <mr.d.sarker@gmail.com>
2026-05-23 17:45:04 +0600

    from office

--- product/api.py
+++ product/api.py
@@ -2,6 +2,24 @@
 from django.http import JsonResponse
 
 from product.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.views import Response
+from rest_framework import viewsets, status
+from .serializers import *
+
+class ProductViewSets(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+@api_view(['GET', 'DELETE'])
+def Product_list_func(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializers = ProductSerializer(products, many=True)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_404_NOT_FOUND)
+    
+
 
 
 def product_api(request):
--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from .api import *
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+# router.register('api', ProductViewSets, basename='pro_viewset')
+
+
 urlpatterns = [
+    path('product/', include(router.urls)),
     # path('', home_page, name='home_page'),
     path('', product_page, name='product_page'),
-    path('api/', product_api, name='product_api'),
-]
+    path('Product_list_func/', Product_list_func, name='Product_list_func'),
+    # path('api/', product_api, name='product_api'),
+] 
